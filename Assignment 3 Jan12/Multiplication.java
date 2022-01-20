2693582ddda99da13be0ef2cc0f22a68101e757f
jhahnavi10 <97846918+jhahnavi10@users.noreply.github.com>
2022-01-21 00:18:42 +0530

    Add files via upload

---
+++ Assignment 3 Jan12/Multiplication.java
@@ -0,0 +1,11 @@
+class Multiplication
+{
+public static void main(String args[])
+{
+	int a=8;
+for(int i=1;i<=10;i++)
+{
+System.out.println(i*a);
+}
+}
+}
---
+++ Assignment 3 Jan12/Temperature.java
@@ -0,0 +1,9 @@
+class Temperature
+{
+public static void main(String args[])
+{
+float f=212.0f;
+float celsius=(f-32)*5/9;
+System.out.println(celsius);
+}
+}
