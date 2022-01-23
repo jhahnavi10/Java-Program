bceb668824ab5963228fb4b00b97da3189a8b147
jhahnavi10 <97846918+jhahnavi10@users.noreply.github.com>
2022-01-23 23:08:47 +0530

    Add files via upload

---
+++ Assignment 6 Jan20/Students.java
@@ -0,0 +1,15 @@
+class Students
+{
+	String name; 
+	int roll_no;
+	public static void main(String args[])
+	{
+		
+	
+	Students obj = new Students();
+	obj.name="Jhanu";
+	obj.roll_no=10;
+	System.out.println("Student name : "+obj.name);
+	System.out.println("Roll_no : "+obj.roll_no);
+	}
+}	
---
+++ Assignment 6 Jan20/Triangles.java
@@ -0,0 +1,20 @@
+public class Triangles 
+{
+	public Triangles()
+	{
+	int a=3;
+	int b=4;
+	int c=5;
+	System.out.println("Area = "+(a*b)/2);
+	System.out.println("Perimeter = "+(a+b+c));
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Triangles obj=new Triangles();
+	}
+
+}
